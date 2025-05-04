6fdc7987b4d635338b8cac5d48ea4f0ff6553e5f
Little-Ki <970559705@qq.com>
2025-05-04 10:47:18 +0800

    update

--- Backend/ControlPanel/src/App.vue
+++ Backend/ControlPanel/src/App.vue
@@ -5,21 +5,13 @@
         on: gameStatus.isOpen
       }">
       </div>
-      <div class="router-item icon-home" @click="() => routeTo('index')" :class="{ active: routerName === 'index' }">
-      </div>
-      <div class="router-item icon-ban" @click="() => routeTo('ban')" :class="{ active: routerName === 'ban' }">
-      </div>
-      <div class="router-item icon-picker" @click="() => routeTo('pick')" :class="{ active: routerName === 'pick' }">
-      </div>
-      <div class="router-item icon-wrench" @click="() => routeTo('feature')"
-        :class="{ active: routerName === 'feature' }">
-      </div>
-      <div class="router-item icon-sword" @click="() => routeTo('champ')" :class="{ active: routerName === 'champ' }">
-      </div>
-      <div class="router-item icon-lobby" @click="() => routeTo('game')" :class="{ active: routerName === 'game' }">
-      </div>
-      <div class="router-item icon-info" @click="() => routeTo('about')" :class="{ active: routerName === 'about' }">
-      </div>
+      <router-button icon="home" name="index" />
+      <router-button icon="ban" name="ban" />
+      <router-button icon="picker" name="pick" />
+      <router-button icon="wrench" name="feature" />
+      <router-button icon="sword" name="champ" />
+      <router-button icon="lobby" name="game" />
+      <router-button icon="info" name="about" />
     </div>
     <div class="router-view">
       <router-view v-slot="{ Component }">
@@ -40,9 +32,10 @@ import { computed, onMounted, ref } from 'vue';
 import type { GameStatus } from '@/api/status'
 import Api from './api';
 
+import RouterButton from './components/RouterButton.vue';
+
 const router = useRouter();
 
-const routerName = computed(() => router.currentRoute.value.name);
 
 const gameStatus = ref({} as GameStatus)
 
@@ -50,11 +43,6 @@ const fetchStatus = () => Api.status.get().then(({ data }) => {
   gameStatus.value = data.data
 })
 
-const routeTo = (name: string) => {
-  router.replace({
-    name
-  })
-}
 onMounted(() => {
   setInterval(fetchStatus, 2000);
 })
@@ -77,29 +65,6 @@ onMounted(() => {
   background: #f0f0f0;
   border-right: 1px solid #e0e0e0;
   user-select: none;
-}
-
-.router-item {
-  font-size: 1.25rem;
-  padding: 0.5rem;
-  line-height: 1;
-  border-radius: 0.25rem;
-  margin-bottom: 0.25rem;
-  transition: all 0.25s;
-  user-select: none;
-  -webkit-user-drag: none;
-  cursor: pointer;
-  transition: all 0.5s ease;
-}
-
-.router-item.active {
-  background: #e0e0e0;
-}
-
-
-.router-item:hover,
-.router-item:active {
-  background: #e0e0e0;
 }
 
 .router-view {
